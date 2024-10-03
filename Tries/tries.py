1f9bed31c1f9a43376df2f8b1cc2e0f8537a9e2e
wasjoe1 <joechua247@gmail.com>
2024-10-03 13:09:55 +0800

    Create Tries boilerplate code

---
+++ Tries/tries.py
@@ -0,0 +1,28 @@
+#!/usr/bin.env python3
+
+# can be found in leetcode qns 208
+
+# A tries (aka prefix tree) is a tree data structure used to efficiently store & retrieve keys in a dataset of strings
+# (or the way i interpret is retrieve substrings/ strings from dataset of strings).
+# This can be used in programs such as autocomplete, spellchecker, or even contains() methods.
+
+# Trie implementation
+class Trie():
+  def __init__(self):
+    pass
+  
+  def insert(self, word: str) -> None:
+    pass
+
+  def search(self, word: str) -> bool:
+    pass
+  
+  def startsWith(self, prefix: str) -> bool:
+    pass
+
+def main():
+  # Code to test Trie
+  # refer to leetcode test cases
+
+if name == "__name__":
+  main()
