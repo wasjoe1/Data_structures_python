fbd0aa77b8c7c941e9b04afb86f735aeacc51c8a
wasjoe1 <joechua247@gmail.com>
2024-10-11 18:39:33 +0800

    Draft 2 Remove comments

--- key_value_store/key_val_store.py
+++ key_value_store/key_val_store.py
@@ -27,12 +27,10 @@ class key_value_store:
     def __init__(self):
         self.perm = {}
         self.temp = []
-        # self.in_transaction = False # initially planned to use this, but since nested kv store, it shall not be the boolean to decide whether a value is added to temp store, rather it should be the length of the temp stack
 
     def has_temp_transaction(self):
         return len(self.temp) > 0
     
-    # methods for changing key val store
     def get(self, key):
         if self.has_temp_transaction():
             return self.temp[-1][key]
@@ -53,46 +51,29 @@ class key_value_store:
             if self.perm.get(key):
                 del self.perm[key]
 
-    # methods for changing the staging mechanism
     def begin(self):
-        # adds on an additional transaction to the system
-        # it will add on from either the previous saved temp OR
-        # if there's no temp, it will add on from perm
-        
         if self.has_temp_transaction():
             self.temp.append(self.temp[-1].copy())
         else:
             self.temp.append(self.perm.copy())
     
     def update_storage(self, old_s, new_s):
-        # want to update changes in old to new
-        # if key in old, but not in new, add to new
-        # if key in old and new, make value of new to old
-        # if key not in old, but in new, delete from new
-        
-        # run through twice
         for k, v in old_s.items():
-            new_s[k] = v # add or update values & keys in key-val store
-        
+            new_s[k] = v
         for k in new_s:
             if k not in old_s:
                 del new_s[k]
 
-    def commit(self): # only applicable when there are transactions
-        # pushes the changes inside the temp to the perm DS (ends curr transaction)
-        # if there is temp transactions
+    def commit(self):
         if self.has_temp_transaction():
             if len(self.temp) == 1:
-                self.update_storage(self.temp[-1], self.perm) # update changes from temp to perm
+                self.update_storage(self.temp[-1], self.perm)
             else:
-                self.update_storage(self.temp[-1], self.temp[-2]) # update changes from latest to 2nd latest
-        # if no temp transaction, nth happens
+                self.update_storage(self.temp[-1], self.temp[-2])
     
-    def rollback(self): # only applicable when there are transactions
-        # removes the temp changes (ends curr transaction)
-        # if there is, if will pop the change, else nth happens
+    def rollback(self):
         if self.has_temp_transaction():
-            self.perm.pop() # pops the latest temp changes
+            self.perm.pop()
 
 def main():
     # code to test key value store
